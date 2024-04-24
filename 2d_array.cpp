149b2d3cd735feac3db67a0edef728836154d799
Manish Singh Tomar <manishtomar00mt@gmail.com>
2024-04-24 21:52:27 +0530

    print statement added

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -11,10 +11,26 @@ int main(){
         {4,5,6}, 
         {2,4,8}};//3x3
 
-    cout << brr[2][2]<<endl;
+//row-wise print
+cout << "printing row Wise"<< endl <<endl;
+//outer loop
+for(int i=0; i<3; i++) {
+    //for every row, we need to print value in each column 
+    for(int j=0;j<3;j++){
+        cout<<brr[i][j]<< " ";
+    }
+    cout<< endl;
+}
     
-
-
+cout << "printing Column Wise"<< endl <<endl;
+//column print 
+for(int i=0; i<3; i++) {
+    //for every column, we need to print value in each row 
+    for(int j=0;j<3;j++){
+        cout<<brr[j][i]<< " ";
+    }
+    cout<< endl;
+}
 
     return 0;
 }
