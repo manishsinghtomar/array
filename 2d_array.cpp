f1d6fbd9da601c171d255a6479fd31d34e173ac6
Manish Singh Tomar <manishtomar00mt@gmail.com>
2024-04-24 18:20:03 +0530

    2d array updated

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -3,4 +3,18 @@ using namespace std;
 
 int main(){
     //declare 2D array
+    int arr[3][3];
+
+    //initialisation
+    int brr[3][3] = {
+        {1,2,3}, 
+        {4,5,6}, 
+        {2,4,8}};//3x3
+
+    cout << brr[2][2]<<endl;
+    
+
+
+
+    return 0;
 }
